11eec9d60a5c24a3e583684bb709e95f378e331b
Chris Jiang <haoyu000@hotmail.com>
2017-12-19 15:58:56 -0500

    Advanced View Cosmetics completed!
    Popup window included. Thier buttons need formatting though. Everything else fully functional

--- js/index_controls.js
+++ js/index_controls.js
@@ -207,3 +207,13 @@ $("#show-btn").click(function(){
 $("#close-btn").click(function(){
 	$("#show-details").toggle();
 });
+
+// Selecting inputs highlights text
+$(function () {
+    var focusedElement;
+    $(document).on('focus', 'input', function () {
+        if (focusedElement == this) return; //already focused, return so user can now place cursor at specific point in input.
+        focusedElement = this;
+        setTimeout(function () { focusedElement.select(); }, 50); //select all text in any field on focus for easy re-entry. Delay sightly to allow focus to "stick" before selecting.
+    });
+});
